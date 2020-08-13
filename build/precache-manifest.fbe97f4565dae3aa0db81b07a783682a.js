cbd09dbcd94b1a20cc90cf6b7f4543b743f6a92f
Anand Kejriwal <personallivingmemories@gmail.com>
2020-08-13 20:35:44 +0530

    final changes

---
+++ build/precache-manifest.fbe97f4565dae3aa0db81b07a783682a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "febf2bdb28c96549c88a38acda54b6d1",
+    "url": "/firstreactwebsite/index.html"
+  },
+  {
+    "revision": "a1cbea08caa5be44f9b9",
+    "url": "/firstreactwebsite/static/css/main.a03981de.chunk.css"
+  },
+  {
+    "revision": "4d25123f357b8cec5e11",
+    "url": "/firstreactwebsite/static/js/2.297df797.chunk.js"
+  },
+  {
+    "revision": "9f24ef6519360b99543da37a4a966358",
+    "url": "/firstreactwebsite/static/js/2.297df797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1cbea08caa5be44f9b9",
+    "url": "/firstreactwebsite/static/js/main.48e1df92.chunk.js"
+  },
+  {
+    "revision": "e7bf4dde0b7c92cbefe1",
+    "url": "/firstreactwebsite/static/js/runtime-main.28298e15.js"
+  }
+]);
